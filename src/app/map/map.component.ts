ec82e2d62a1e3abc01eb553e80b167558984cacb
ludobegins <ludo.beghin@gmail.com>
2023-10-15 15:12:16 -0300

    addLocationsMarkers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Map, NavigationControl } from 'mapbox-gl'
+import { Map, NavigationControl, Marker, Popup } from 'mapbox-gl'
 import { getDatabase, ref, onValue, get, child } from "firebase/database"; // https://firebase.google.com/docs/database/web/start
 
 @Component({
@@ -12,30 +12,38 @@ export class MapComponent implements OnInit {
 
   private mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
   private map: any;
+  private locations: any; // tipar depois que definir direito na db
   
-  ngOnInit(): void {
+  async ngOnInit() {
     this.mapboxgl.accessToken = environment.mapbox.accessToken;
     this.map = new Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/outdoors-v11'
     });
     this.map.addControl(new NavigationControl());
-    this.fetchDbData();
-  }
+    await this.fetchDbData();
+    this.addLocationsMarkers();
+  };
 
-  fetchDbData() { 
+  async fetchDbData() { 
     const db = getDatabase();
     const dbRef = ref(db);
 
-    get(child(dbRef, 'locations')).then((snapshot) => {
-        if (snapshot.exists()) {
-          console.log(snapshot.val());
-        } else {
-          console.log("No locations");
-        }
-      }).catch((error) => {
-        console.error(error);
-      });
-  }
+    let snapshot = await get(child(dbRef, 'locations'));
+    if (snapshot.exists()) {
+      this.locations = snapshot.val();
+      return;
+    };
+    console.warn('No locations');
+  };
+
+  addLocationsMarkers(){
+    for (let locationId of Object.keys(this.locations)){
+      const marker = new Marker()
+        .setLngLat([this.locations[locationId].coordinates.longitude, this.locations[locationId].coordinates.latitude])
+        .setPopup(new Popup().setHTML("<h1>Hello World!</h1>"))
+        .addTo(this.map);
+    }
+  };
 
 }
