1afea378b303e2357301779fec069b50a77aa43b
ludobegins <ludo.beghin@gmail.com>
2023-10-15 18:32:54 -0300

    locations imgs in popups

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Map, NavigationControl, Marker, Popup } from 'mapbox-gl'
-import { getDatabase, ref, onValue, get, child } from "firebase/database"; // https://firebase.google.com/docs/database/web/start
+import { firebaseConfig } from '../../../env'
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref as ref_db, onValue, get, child } from "firebase/database"; // https://firebase.google.com/docs/database/web/start
+import { getStorage, ref as ref_storage, listAll, getDownloadURL } from "firebase/storage";
 
 interface Location {
   name: string,
@@ -31,6 +34,8 @@ export class MapComponent implements OnInit {
   private mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
   private map!: Map;
   private locations!: Locations;
+  private app: any;
+  private storage: any;
   
   async ngOnInit() {
     this.mapboxgl.accessToken = environment.mapbox.accessToken;
@@ -39,13 +44,15 @@ export class MapComponent implements OnInit {
       style: 'mapbox://styles/mapbox/outdoors-v11'
     });
     this.map.addControl(new NavigationControl());
+    this.app = initializeApp(firebaseConfig);
     await this.fetchDbData();
     this.addLocationsMarkers();
+
   };
 
   async fetchDbData() { 
     const db = getDatabase();
-    const dbRef = ref(db);
+    const dbRef = ref_db(db);
 
     let snapshot = await get(child(dbRef, 'locations'));
     if (snapshot.exists()) {
@@ -55,14 +62,29 @@ export class MapComponent implements OnInit {
     console.warn('No locations');
   };
 
-  addLocationsMarkers(){
+  async addLocationsMarkers(){
     for (let id of Object.keys(this.locations)){
-      let popupHtml = `<h1>${this.locations[id].name}</h1> <div>${this.locations[id].description}</div> <h4>Primeira visita: ${this.locations[id].date}<\h4>`
+      const imgUrl = await this.getLocationImgUrl(id);
+      let popupHtml = `<h1>${this.locations[id].name}</h1><img width="200" height="120" src=${imgUrl}> <div>${this.locations[id].description}</div> <h4>Primeira visita: ${this.locations[id].date}<\h4>`
       const marker = new Marker()
         .setLngLat([this.locations[id].coordinates.longitude, this.locations[id].coordinates.latitude])
         .setPopup(new Popup().setHTML(popupHtml))
         .addTo(this.map);
     }
   };
+
+  async getLocationImgUrl(locationId: string): Promise<string>{
+    this.storage = getStorage(this.app);
+    const listRef = ref_storage(this.storage, `locations/${locationId}`);
+
+    let imgPath = '';
+    let res = await listAll(listRef)
+    res.items.forEach((itemRef) => {
+      imgPath = itemRef.fullPath
+    });
+
+    let url = await getDownloadURL(ref_storage(this.storage, imgPath))
+    return url;
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from '../env'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
 .catch(err => console.error(err));
-
-const app = initializeApp(firebaseConfig);
-
