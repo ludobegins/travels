28bd975f8da292b2f0121c2b8c53fbd099ff9697
ludobegins <ludo.beghin@gmail.com>
2023-10-15 12:28:45 -0300

    init map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+
+  private mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+  private map = new Map();
+  
+  ngOnInit(): void {
+    this.mapboxgl.accessToken = environment.mapbox.accessToken;
+    let map = new this.mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+}
 
 }
